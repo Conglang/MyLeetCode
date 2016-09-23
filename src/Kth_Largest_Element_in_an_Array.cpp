e4e425a89d31c4964e6d34816d0a3aad2d8cb03c
Conglang <yanru.conglang@gmail.com>
2016-09-23 14:59:42 +0800

    Today's share on Devide and Conquer.

---
+++ src/Kth_Largest_Element_in_an_Array.cpp
@@ -0,0 +1,57 @@
+//////////////////////////////////////////////////////
+//		Project:		MyLeetCode
+//
+//		Author:			YanShicong
+//		Date:			2016/09/23
+//////////////////////////////////////////////////////
+/*--------------------------------------------------------------------------------------------------------------
+* Find the kth largest element in an unsorted array.
+* Note that it is the kth largest element in the sorted order, not the kth distinct element.
+* 
+* For example,
+* Given [3,2,1,5,6,4] and k = 2, return 5.
+* 
+* Note: 
+* You may assume k is always valid, 1 ¡Ü k ¡Ü array's length.
+//--------------------------------------------------------------------------------------------------------------*/
+#include "../include/include.h"
+// quick sort
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        return findKthLargest(nums, 0, nums.size(), k-1);
+    }
+private:
+    int findKthLargest(vector<int>& nums, int start, int end, int k)
+    {
+        int pos = partition(nums, start, end);
+        if (pos-start == k)     {return nums[pos];}
+        else if (pos-start > k) {return findKthLargest(nums, start, pos, k);}
+        else                    {return findKthLargest(nums, pos+1, end, k-(pos-start)-1);}
+    }
+    int partition(vector<int>& nums, int start, int end)
+    {
+        int curValue = nums[start];
+        int left(start+1), right(end-1);
+        while (left <= right)
+        {
+            while(left <= right && nums[left] > curValue) {++left;}
+            while(left <= right && nums[right] <= curValue) {--right;}
+            if (left < right)
+            {
+                swap(nums[left], nums[right]);
+            }
+        }
+        if (right > start)
+            swap(nums[start], nums[right]);
+        return right;
+    }
+};
+//--------------------------------------------------------------------------------------------------------------
+TEST_CASE("Kth_Largest_Element_in_an_Array", "[Devide and Conquer]") {
+	Solution s;
+	SECTION("Normal Input") {
+		int a1[7] = {7,6,5,4,3,2,1};
+		REQUIRE(s.findKthLargest(vector<int>(a1,a1+7), 5) == 3);
+	}
+}
---
+++ src/Majority_Element.cpp
@@ -0,0 +1,60 @@
+﻿//////////////////////////////////////////////////////
+//		Project:		MyLeetCode
+//
+//		Author:			YanShicong
+//		Date:			2016/09/23
+//////////////////////////////////////////////////////
+/*--------------------------------------------------------------------------------------------------------------
+* Given an array of size n, find the majority element.
+* The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+* 
+* You may assume that the array is non-empty and the majority element always exist in the array.
+//--------------------------------------------------------------------------------------------------------------*/
+#include "../include/include.h"
+#define W2
+
+#ifdef W1
+// 排序，比较慢。
+// 时间复杂度O(nlogn)，空间复杂度O(1)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return *(nums.begin() + nums.size()/2);
+    }
+};
+#endif
+
+#ifdef W2
+// Ref: http://www.cs.utexas.edu/~moore/best-ideas/mjrty/
+// 时间复杂度O(n)，空间复杂度O(1)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majorElem(nums[0]), count(1);
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            if (count == 0)
+            {
+                majorElem = nums[i];
+                ++count;
+            }else if (majorElem == nums[i])
+            {
+                ++count;
+            }else
+            {
+                --count;
+            }
+        }
+        return majorElem;
+    }
+};
+#endif
+//--------------------------------------------------------------------------------------------------------------
+TEST_CASE("Majority_Element", "[Devide and Conquer]"){
+	Solution s;
+	SECTION("Normal Input"){
+		int a1[9] = {1,5,3,4,5,6,5,5,5};
+		REQUIRE(s.majorityElement(vector<int>(a1,a1+9)) == 5);
+	}
+}
